f008be3e7b8122c5409aef70fdfdebc9c97e0d4b
asranim <asrani.27@gmail.com>
2019-12-28 14:15:05 +0800

    fix

--- resources/views/includes/menu_superadmin.blade.php
+++ resources/views/includes/menu_superadmin.blade.php
@@ -16,6 +16,20 @@
         <li class="{{ (request()->is('masterdata/jabatan')) ? 'active' : '' }}"><a href="/masterdata/jabatan">Jabatan</a></li>
     </ul>
 </li>
+<li class="has-sub {{ (request()->is('masterdata*')) ? 'active' : '' }}">
+    <a href="javascript:;">
+        <b class="caret"></b>
+        <i class="fa fa-th-large"></i>
+        <span>Report Data</span>
+    </a>
+    <ul class="sub-menu">
+        <li class="{{ (request()->is('masterdata/sekolah')) ? 'active' : '' }}"><a href="#">Lap Sekolah</a></li>
+        <li class="{{ (request()->is('masterdata/jabatan')) ? 'active' : '' }}"><a href="#">Lap Pegawai</a></li>
+        <li class="{{ (request()->is('masterdata/jabatan')) ? 'active' : '' }}"><a href="#">Lap Siswa</a></li>
+        <li class="{{ (request()->is('masterdata/jabatan')) ? 'active' : '' }}"><a href="#">Lap Jumlah Pegawai Semua</a></li>
+        <li class="{{ (request()->is('masterdata/jabatan')) ? 'active' : '' }}"><a href="#">Lap Jumlah Siswa Semua</a></li>
+    </ul>
+</li>
 <li class="{{ (request()->is('kelola_user')) ? 'active' : '' }}">
     <a href="/kelola_user">
         <i class="fa fa-th-large"></i> 
